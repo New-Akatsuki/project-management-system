7c7c22878ff5aab7957c29b2c6cb5be059ee5312
Myat-Noe-9 <myatnoee2004@gmail.com>
2023-10-20 15:05:52 +0630

    all user views created

--- src/main/java/org/blank/projectmanagementsystem/controller/HomeController.java
+++ src/main/java/org/blank/projectmanagementsystem/controller/HomeController.java
@@ -9,4 +9,28 @@ public class HomeController {
     public String index(){
         return "index";
     }
+    @GetMapping("/userview")
+    public String userview(){
+        return "userview";
+
+    }
+    @GetMapping("/adduser")
+    public String adduser(){
+        return "adduser";
+
+    }
+    @GetMapping("/userprofile")
+    public String userprofile(){
+        return "userprofile";
+
+    }
+
+    @GetMapping("/createproject")
+    public String createproject(){
+        return "createproject";
+
+    }
+
+
+
 }
---
+++ src/main/resources/static/js/validation.js
@@ -0,0 +1,40 @@
+function checkInputValidation(element_id, element_name, min_length) {
+    const nameInput = document.getElementById(element_id);
+    const msg_div = document.getElementById(`${element_id}_error`);
+
+    nameInput.addEventListener("blur", function () {
+        if (!nameInput.validity.valid) {
+            if (nameInput.validity.valueMissing) {
+                nameInput.setCustomValidity(`${element_name||""} is required.`);
+            }else if(nameInput.validity.patternMismatch){
+                nameInput.setCustomValidity(`Please enter valid ${element_name||""}`)
+            } else if (nameInput.validity.tooShort) {
+                nameInput.setCustomValidity(`${element_name} must be at least ${min_length||0} characters.`);
+            }
+            nameInput.classList.add("is-invalid");
+            msg_div.innerText = nameInput.validationMessage;
+        }
+    });
+
+    nameInput.addEventListener("input", function () {
+        nameInput.setCustomValidity(""); // Clear custom validation message
+        if (nameInput.validity.valid) {
+            nameInput.classList.remove("is-invalid");
+        }else{
+            nameInput.classList.add("is-valid");
+        }
+    });
+
+}
+
+function formValidate(form_id){
+    const nameForm = document.getElementById(form_id);
+
+    nameForm.addEventListener("submit", function (event) {
+        if (!nameForm.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        nameForm.classList.add("was-validated");
+    });
+}
