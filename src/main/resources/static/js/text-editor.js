99dbb6d9006899f99d7ecb29dba929cb803ba0ab
MMinHein <145110055+MMinHein@users.noreply.github.com>
2023-10-20 11:28:43 +0630

    Ui Test 2

--- src/main/resources/static/js/text-editor.js
+++ src/main/resources/static/js/text-editor.js
@@ -25,3 +25,50 @@ let aa = {
     theme: "snow"
 }
 
+function toggleToolbar(index, editor_id) {
+    const containers = document.querySelectorAll('.ql-container');
+    const toolbars = document.querySelectorAll('.ql-toolbar');
+    const textEditorContainer = document.getElementById(editor_id);
+
+    const showToolbar = () => {
+        toolbars[index].style.display = 'block';
+        toolbars[index].style.border = '1px solid #ccc';
+        containers[index].classList.remove('round-full-border');
+    };
+
+    const hideToolbar = () => {
+        toolbars[index].style.display = 'none';
+        containers[index].classList.add('round-full-border');
+    };
+
+    const hidePreviousToolbars = () => {
+        toolbars.forEach((toolbar, i) => {
+            if (i !== index) {
+                toolbar.style.display = 'none';
+                containers[i].classList.add('round-full-border');
+            }
+        });
+    };
+
+    textEditorContainer.addEventListener('click', (event) => {
+        console.log('clicked text editor');
+        hidePreviousToolbars();
+        showToolbar();
+        event.stopPropagation(); // Prevent the event from reaching the document click handler immediately
+    });
+
+    document.addEventListener('click', (event) => {
+        // Check if the clicked element is outside the text editor and toolbar
+        if (!textEditorContainer.contains(event.target) && !toolbars[index].contains(event.target)) {
+            hideToolbar();
+        }
+    });
+
+    // Update hidden inputs or perform other actions when clicking inside the text editor
+    document.addEventListener('click', updateHiddenInputs);
+
+    // If you want to stop propagation when clicking inside the toolbar
+    toolbars[index].addEventListener('click', (event) => {
+        event.stopPropagation();
+    });
+}
