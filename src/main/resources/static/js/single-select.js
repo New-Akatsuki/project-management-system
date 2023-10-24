51fd283e17e8556d6abd74c0b11c63745678d2b5
KhunAp <khunaungpaing.it@gmail.com>
2023-10-24 09:39:34 +0630

    modify single select

--- src/main/resources/static/js/single-select.js
+++ src/main/resources/static/js/single-select.js
@@ -1,12 +1,12 @@
-let data=[];
+let searchbar;
+
 function singleSelect(id, input_data, create_new_option={btn_id:'',modal_id:null,btn_name:'Add new'}) {
     const wrapper = document.getElementById(id),
         selectBtn = wrapper.querySelector(`#${id} .select-btn`),
         searchInp = wrapper.querySelector(`#${id} input`),
         options = wrapper.querySelector(`#${id} .options`);
-    data = Array.isArray(input_data) ? input_data : [input_data];
+    let data = Array.isArray(input_data) ? input_data : [input_data];
     addCountry(options);
-
     function addCountry(selectedItem) {
         options.innerHTML = "";
         data.forEach(item => {
@@ -18,9 +18,11 @@ function singleSelect(id, input_data, create_new_option={btn_id:'',modal_id:null
 
     function updateName(selectedLi) {
         searchInp.value = "";
-        addCountry(selectedLi.innerText);
-        wrapper.classList.remove("active");
-        selectBtn.firstElementChild.innerText = selectedLi.innerText;
+        if(selectedLi){
+            addCountry(selectedLi.innerText);
+            wrapper.classList.remove("active");
+            selectBtn.firstElementChild.innerText = selectedLi.innerText;
+        }
     }
 
     window.addEventListener('click', (e) => {
@@ -31,6 +33,7 @@ function singleSelect(id, input_data, create_new_option={btn_id:'',modal_id:null
 
 
     searchInp.addEventListener("keyup", () => {
+        searchbar = searchInp;
         let arr = [];
         let searchWord = searchInp.value.toLowerCase();
         arr = data.filter(data => {
@@ -42,7 +45,7 @@ function singleSelect(id, input_data, create_new_option={btn_id:'',modal_id:null
         if(create_new_option.modal_id){
             options.innerHTML = arr ? arr :
                 `<p style="margin-top: 10px;">
-                     <a id="${create_new_option.btn_id}" onclick="resetSearchInp(${searchInp})" class="icon-add-btn" data-toggle="modal" data-target="#${create_new_option.modal_id}">
+                     <a id="${create_new_option.btn_id}" class="icon-add-btn" data-toggle="modal" data-target="#${create_new_option.modal_id}">
                         <i class='bx bx-plus m-0 p-0'></i> ${create_new_option.btn_name||'Add new'}
                      </a>
                 </p>`;
@@ -50,19 +53,18 @@ function singleSelect(id, input_data, create_new_option={btn_id:'',modal_id:null
         else {
             options.innerHTML = arr ? arr : `<p style="margin-top: 10px;">Not found.</p>`;
         }
-
     });
 
     selectBtn.addEventListener("click", () => wrapper.classList.toggle("active"));
 
     return updateName;
 }
-function resetSearchInp(searchInp){
-    searchInp.value = "";
-}
 
 function addNewOption(new_option){
-    data = [...data,new_option];
+    searchbar.value = null
+    if(new_option){
+
+    }
 }
 
 function init(id,placeholder){
