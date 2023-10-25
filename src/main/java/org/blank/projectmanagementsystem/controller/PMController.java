dc02d25d293d7a9b9e5eef489bdda9515f3cadf7
Sar Sar <sarsar.catsdontcare@gmail.com>
2023-10-25 15:49:13 +0630

    25.10

--- src/main/java/org/blank/projectmanagementsystem/controller/PMController.java
+++ src/main/java/org/blank/projectmanagementsystem/controller/PMController.java
@@ -2,11 +2,36 @@ package org.blank.projectmanagementsystem.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/pm")
 public class PMController {
+
+    @GetMapping("/project-detail")
+    public String ProjectDetail(){
+        return "project-detail";
+    }
+
+    @GetMapping("/project-list")
+    public String ProjectList(){
+        return "project-list";
+    }
+
+    @GetMapping("/contract-info")
+    public String ContractInfo(){
+        return "contract-info";
+    }
+
+    @GetMapping("/department")
+    public String Department(){
+        return "department";
+    }
+
+    @GetMapping("/user-list")
+    public String UserList(){
+        return "user-list";
+    }
 
 }
