f3b04ea0041b58157423ec9c8b3234a573c36893
KhunAp <khunaungpaing.it@gmail.com>
2023-10-25 23:08:07 +0630

    add project-detail.html

--- src/main/java/org/blank/projectmanagementsystem/controller/PMController.java
+++ src/main/java/org/blank/projectmanagementsystem/controller/PMController.java
@@ -8,5 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 @RequestMapping("/pm")
 public class PMController {
+    @RequestMapping("/project-details-info")
+    public String dashboard() {
+        return "project-details-info";
+    }
 
 }
