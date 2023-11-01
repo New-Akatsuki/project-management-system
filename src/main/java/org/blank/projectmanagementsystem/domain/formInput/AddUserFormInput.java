93b614d0160bd59c0dad57a7ccdc01bee1921c53
Myat-Noe-9 <myatnoee2004@gmail.com>
2023-11-01 11:16:45 +0630

    modified function user register

--- src/main/java/org/blank/projectmanagementsystem/domain/formInput/AddUserFormInput.java
+++ src/main/java/org/blank/projectmanagementsystem/domain/formInput/AddUserFormInput.java
@@ -4,9 +4,10 @@ import lombok.Data;
 
 @Data
 public class AddUserFormInput {
-    private String name;
+
     private String username;
+    private String email;
     private String role;
     private String department;
-    private String image;
+
 }
--- src/main/java/org/blank/projectmanagementsystem/mapper/UserMapper.java
+++ src/main/java/org/blank/projectmanagementsystem/mapper/UserMapper.java
@@ -2,7 +2,7 @@ package org.blank.projectmanagementsystem.mapper;
 
 import lombok.RequiredArgsConstructor;
 import org.blank.projectmanagementsystem.domain.entity.Department;
-import org.blank.projectmanagementsystem.domain.entity.Role;
+import org.blank.projectmanagementsystem.domain.Enum.Role;
 import org.blank.projectmanagementsystem.domain.entity.User;
 import org.blank.projectmanagementsystem.domain.formInput.AddUserFormInput;
 import org.blank.projectmanagementsystem.domain.viewobject.UserViewObject;
